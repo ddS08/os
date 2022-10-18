17a64ecde2f1b8fecea7cecb162930c9ac40b433
Dhyaan Shah <80191264+ddS08@users.noreply.github.com>
2022-10-18 11:24:55 +0530

    Update RoundRobin.java

--- RoundRobin.java
+++ RoundRobin.java
@@ -1,146 +1,114 @@
-package os;
+# Python3 program for implementation of
+# RR scheduling
 
-import java.util.*;
+# Function to find the waiting time
+# for all processes
+def findWaitingTime(processes, n, bt,
+						wt, quantum):
+	rem_bt = [0] * n
 
-public class RoundRobin{
-    private static Scanner sc = new Scanner(System.in);
-    //Driver Code
-    public static void main(String[] args){
-        int n,tq, timer = 0, maxProccessIndex = 0;
-        float avgWait = 0, avgTT = 0;
-        System.out.print("\nEnter the time quanta : ");
-        tq = sc.nextInt();
-        System.out.print("\nEnter the number of processes : ");
-        n = sc.nextInt();
-        String pid[] = new String[n];
-        int arrival[] = new int[n];
-        int burst[] = new int[n];
-        int wait[] = new int[n];
-        int turn[] = new int[n];
-        int queue[] = new int[n];
-        int temp_burst[] = new int[n];
-        int completiontime[] = new int[n];
-        boolean complete[] = new boolean[n];
-        for (int i=0;i<n;i++)
-        {
-            pid[i]= "P"+(i+1);
-            System.out.println ("enter process " +(i+1)+ " arrival time:");
-            arrival[i]= sc.nextInt();
-            System.out.println("enter process " +(i+1)+ " burst time:");
-            burst[i]= sc.nextInt();
-            temp_burst[i] = burst[i];
-        }
+	# Copy the burst time into rt[]
+	for i in range(n):
+		rem_bt[i] = bt[i]
+	t = 0 # Current time
 
-        for(int i = 0; i < n; i++){    //Initializing the queue and complete array
-            complete[i] = false;
-            queue[i] = 0;
-        }
-        while(timer < arrival[0])    //Incrementing Timer until the first process arrives
-            timer++;
-        queue[0] = 1;
+	# Keep traversing processes in round
+	# robin manner until all of them are
+	# not done.
+	while(1):
+		done = True
 
-        while(true){
-            boolean flag = true;
-            for(int i = 0; i < n; i++){
-                if(temp_burst[i] != 0){
-                    flag = false;
-                    break;
-                }
-            }
-            if(flag)
-                break;
+		# Traverse all processes one by
+		# one repeatedly
+		for i in range(n):
+			
+			# If burst time of a process is greater
+			# than 0 then only need to process further
+			if (rem_bt[i] > 0) :
+				done = False # There is a pending process
+				
+				if (rem_bt[i] > quantum) :
+				
+					# Increase the value of t i.e. shows
+					# how much time a process has been processed
+					t += quantum
 
-            for(int i = 0; (i < n) && (queue[i] != 0); i++){
-                int ctr = 0;
-                while((ctr < tq) && (temp_burst[queue[0]-1] > 0)){
-                    temp_burst[queue[0]-1] -= 1;
-                    timer += 1;
-                    ctr++;
+					# Decrease the burst_time of current
+					# process by quantum
+					rem_bt[i] -= quantum
+				
+				# If burst time is smaller than or equal
+				# to quantum. Last cycle for this process
+				else:
+				
+					# Increase the value of t i.e. shows
+					# how much time a process has been processed
+					t = t + rem_bt[i]
 
-                    //Updating the ready queue until all the processes arrive
-                    checkNewArrival(timer, arrival, n, maxProccessIndex, queue);
-                }
-                if((temp_burst[queue[0]-1] == 0) && (complete[queue[0]-1] == false)){
-                    turn[queue[0]-1] = timer;        //turn currently stores exit times
-                    complete[queue[0]-1] = true;
-                }
+					# Waiting time is current time minus
+					# time used by this process
+					wt[i] = t - bt[i]
 
-                //checks whether or not CPU is idle
-                boolean idle = true;
-                if(queue[n-1] == 0){
-                    for(int k = 0; k < n && queue[k] != 0; k++){
-                        if(complete[queue[k]-1] == false){
-                            idle = false;
-                        }
-                    }
-                }
-                else
-                    idle = false;
+					# As the process gets fully executed
+					# make its remaining burst time = 0
+					rem_bt[i] = 0
+				
+		# If all processes are done
+		if (done == True):
+			break
+			
+# Function to calculate turn around time
+def findTurnAroundTime(processes, n, bt, wt, tat):
+	
+	# Calculating turnaround time
+	for i in range(n):
+		tat[i] = bt[i] + wt[i]
 
-                if(idle){
-                    timer++;
-                    checkNewArrival(timer, arrival, n, maxProccessIndex, queue);
-                }
 
-                //Maintaining the entries of processes after each premption in the ready Queue
-                queueMaintainence(queue,n);
-            }
-        }
+# Function to calculate average waiting
+# and turn-around times.
+def findavgTime(processes, n, bt, quantum):
+	wt = [0] * n
+	tat = [0] * n
 
-        for(int i = 0; i < n; i++){
+	# Function to find waiting time
+	# of all processes
+	findWaitingTime(processes, n, bt,
+						wt, quantum)
 
-            turn[i] = turn[i] - arrival[i];
-            wait[i] = turn[i] - burst[i];
-            completiontime[i] = turn[i]+arrival[i];
-        }
-        System.out.println("\nProcessid Arrival time(AT) burst time(BT) completion time(CT) turnaround time(TAT) Waiting time(WT)");
-        for(int i = 0 ;i < n;i++)
-        {
-            System.out.println("\t"+pid[i] +"\t\t\t"+ arrival[i]+"\t\t\t\t"+burst[i]+"\t\t\t\t"+completiontime[i]+"\t\t\t\t\t"+turn[i]+"\t\t\t\t\t\t"+wait[i]);
-        }
-        System.out.println();
-        for(int i =0; i< n; i++){
-            avgWait += wait[i];
-            avgTT += turn[i];
-        }
-        System.out.print("\nAverage wait time : "+(avgWait/n)
-                +"\nAverage Turn Around Time : "+(avgTT/n));
-    }
-    public static void queueUpdation(int queue[],int timer,int arrival[],int n, int maxProccessIndex){
-        int zeroIndex = -1;
-        for(int i = 0; i < n; i++){
-            if(queue[i] == 0){
-                zeroIndex = i;
-                break;
-            }
-        }
-        if(zeroIndex == -1)
-            return;
-        queue[zeroIndex] = maxProccessIndex + 1;
-    }
+	# Function to find turn around time
+	# for all processes
+	findTurnAroundTime(processes, n, bt,
+								wt, tat)
 
-    public static void checkNewArrival(int timer, int arrival[], int n, int maxProccessIndex,int queue[]){
-        if(timer <= arrival[n-1]){
-            boolean newArrival = false;
-            for(int j = (maxProccessIndex+1); j < n; j++){
-                if(arrival[j] <= timer){
-                    if(maxProccessIndex < j){
-                        maxProccessIndex = j;
-                        newArrival = true;
-                    }
-                }
-            }
-            if(newArrival)    //adds the index of the arriving process(if any)
-                queueUpdation(queue,timer,arrival,n, maxProccessIndex);
-        }
-    }
+	# Display processes along with all details
+	print("Processes Burst Time	 Waiting",
+					"Time Turn-Around Time")
+	total_wt = 0
+	total_tat = 0
+	for i in range(n):
 
-    public static void queueMaintainence(int queue[], int n){
+		total_wt = total_wt + wt[i]
+		total_tat = total_tat + tat[i]
+		print(" ", i + 1, "\t\t", bt[i],
+			"\t\t", wt[i], "\t\t", tat[i])
 
-        for(int i = 0; (i < n-1) && (queue[i+1] != 0) ; i++){
-            int temp = queue[i];
-            queue[i] = queue[i+1];
-            queue[i+1] = temp;
-        }
-    }
-}
+	print("\nAverage waiting time = %.5f "%(total_wt /n) )
+	print("Average turn around time = %.5f "% (total_tat / n))
+	
+# Driver code
+if __name__ =="__main__":
+	
+	# Process id's
+	proc = [1, 2, 3]
+	n = 3
+
+	# Burst time of all processes
+	burst_time = [10, 5, 8]
+
+	# Time quantum
+	quantum = 2;
+	findavgTime(proc, n, burst_time, quantum)
+
+# This code is contributed by
+# Shubham Singh(SHUBHAMSINGH10)
